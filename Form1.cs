5a89e24133b1e2fafd0fd5a7c0b0a61e0814f6c4
poyry4 <53422476+poyry4@users.noreply.github.com>
2021-12-12 13:32:16 -0800

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Template
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void Register(human form)
+        {
+            if (form.Age >= 18)
+            {
+                Db db1 = new Db();
+                db1.humen.Add(form);
+                db1.SaveChanges();
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Register(new human { Name = textBox1.Text, Family = textBox2.Text, Age = byte.Parse(textBox3.Text) });
+        }
+    }
+}
---
+++ human.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template
+{
+    class human
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Family { get; set; }
+        public byte Age { get; set; }
+    }
+}
